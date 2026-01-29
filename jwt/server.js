389126e9363e2dd0657b498eeb28fd3b4b9a91aa
Guilherme Louback <luisguilhermels21@gmail.com>
2026-01-29 06:55:22 -0300

    JSON WEB TOKEN fully implemented and working

--- jwt/server.js
+++ jwt/server.js
@@ -10,17 +10,17 @@ app.use(express.json());
 
 const posts = [
     {
-        user: "Kyle",
+        username: "Kyle",
         title: "Post 1" 
     },  
     {
-        user: "Jim",
+        username: "Jim",
         title: "Post 2"
     }
 ];
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
+app.get('/posts', authenticateToken, (req, res) => {
+    res.send(posts.filter(post => post.username === req.user.name));
 });
 
 app.post('/login', (req, res) => {
@@ -34,6 +34,35 @@ app.post('/login', (req, res) => {
         accessToken: accessToken
     });
 });
+
+// testig jwt
+function authenticateToken(req, res, next) {
+    // get and verify token and then return user to get('/posts')
+
+    /* 
+        the token will come from the request headers in the format bellow
+
+        Bearer TOKEN
+
+        Bearer is a header, the authorization header 
+    */
+    
+    // getting header
+    const authHeader = req.headers['authorization'];
+
+    // getting token and check authHeader
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    // verifying token with signature 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+
+        // passing user to req.user and calling next middleware in the stack
+        req.user = user;
+        next();
+    });
+}
 
 app.listen(3000, () => {
     console.log('server running on http://localhost:3000')
